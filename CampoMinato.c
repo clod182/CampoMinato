6b5626ceaca2143cfb147ebf3ca6fa5929e7fbb6
Claudio Manesso_fisso <clod182@hotmail.com>
2017-05-06 18:41:57 +0200

    Start 0.0

---
+++ CampoMinato.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	int scelta=0;
+	printf("***---***CampoMinato***---***\n");
+	printf("Cosa vuoi fare?\n1 - Generare schema tramite input\n2 - Generare schema tramite file\n3 - Uscita\n");
+	scanf("%d",&scelta);
+	if (scelta==1){
+		int h,l;
+		printf("Dammi la dimensione del campo (h l)\n");
+	}
+	return 0;
+}
