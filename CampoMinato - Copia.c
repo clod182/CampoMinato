872d2ccb688efa4dcf093a62422357c74f05137c
Claudio Manesso_fisso <clod182@hotmail.com>
2017-05-07 15:16:33 +0200

    wip0.2
    Creazione del campo migliorata e implementato inserimento delle mine

--- CampoMinato - Copia.c
+++ CampoMinato - Copia.c
@@ -1,8 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-
-
 char** creaCampo(char** campo,int r,int c){
 	int i,j;
 	campo=(char**)malloc(r*sizeof(char*));
@@ -12,70 +10,26 @@ char** creaCampo(char** campo,int r,int c){
 	return campo;
 }
 
-
 char** riempiCampo(char** campo,int r, int c){
 	int i,j;
 	for(i=0;i<r;i++){
 		for(j=0;j<c;j++){
 			campo[i][j]='0';
 		}
-	}	
+	}
 	return campo;
 }
-
 
 void stampaCampo(char** campo,int r,int c){
 	int i,j;
 	printf("-----Il campo e questo-----\n");
-	/*for(i=0;i<r;i++){
+	for(i=0;i<r;i++){
 		for(j=0;j<c;j++){
 			printf("%c",campo[i][j]);
 		}
 		printf("\n");
-	}*/
-	for(j=0;j<c;j++){
-		printf("%d  ",j+1);
 	}
-	printf("\n");
-	for(j=0;j<c;j++){
-		printf("-  ",j+1);
-	}	
-	printf("\n\n");
-    for(i=0;i<r;i++){
-        for(j=0;j<c;j++){
-            printf("%c  ",campo[i][j]);
-        }
-    	printf("|%d ",i+1);
-    	printf("\n");
-    }
 }
-
-
-char** inserisciMine(char** campo,int r,int c,int m){
-	int i,j,ran1,ran2,contm=0;
-	srand(time(NULL));
-	/*for(i=0;i<r && contm<=m;i++){
-		for(j=0;j<c && contm<=m;j++){
-			ran1=rand()%(r);
-			ran2=rand()%(c);
-			if(campo[ran1][ran2]!='*'){
-				campo[ran1][ran2]='*';
-				contm++;
-			}							
-		}
-	}*/	
-	while(contm<m){
-		ran1=rand()%(r);
-		ran2=rand()%(c);
-		if(campo[ran1][ran2]!='*'){
-			campo[ran1][ran2]='*';
-			contm++;
-		}		
-	}
-	return campo;
-}
-
-
 int main(){
 	int scelta=0;
 	char** CampoMain;
@@ -89,14 +43,12 @@ int main(){
 			scanf("%d %d",&R, &C);
 		}while(R<=0 || C<=0);		
 		CampoMain=creaCampo(CampoMain,R,C);
-		CampoMain=riempiCampo(CampoMain,R,C);				
+		CampoMain=riempiCampo(CampoMain,R,C);
 		stampaCampo(CampoMain,R,C);
 		do{
 			printf("Dammi il numero di mine che vuoi inserire (0<n<dimensione campo -1)\n");
 			scanf("%d",&mine);
 		}while(mine<=0 || mine>(R*C)-1);
-		CampoMain=inserisciMine(CampoMain,R,C,mine);
-		stampaCampo(CampoMain,R,C);
 	}
 	return 0;
 }
