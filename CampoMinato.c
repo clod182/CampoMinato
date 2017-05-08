191ec0434ea2841f91402ed1359fa45980bbf235
Claudio Manesso_fisso <clod182@hotmail.com>
2017-05-08 17:16:22 +0200

    wip0.3
    -implemetata la funzione turno giocatore in fase primordiale
    -wip sulla funzione che inserisce i numeri delle mine adiacenti

--- CampoMinato.c
+++ CampoMinato.c
@@ -1,7 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-
+#define MINA '*'
+#define VUOTO '0'
 
 char** creaCampo(char** campo,int r,int c){
 	int i,j;
@@ -13,14 +14,39 @@ char** creaCampo(char** campo,int r,int c){
 }
 
 
-char** riempiCampo(char** campo,int r, int c){
+char** riempiCampo(char** campo,int r, int c, char carattere){
 	int i,j;
 	for(i=0;i<r;i++){
 		for(j=0;j<c;j++){
-			campo[i][j]='0';
+			campo[i][j]=carattere;
 		}
 	}	
 	return campo;
+}
+
+
+void aggiungiNumeri(char** campo,int r, int c){
+	int i,j,k,kk;
+	char conta='0';
+	for(i=0;i<r;i++){
+		for(j=0;j<c;j++){
+			conta='0';
+			if(campo[i][j]!=MINA && campo[i][j]==VUOTO){
+				/*for(k=(i-1);k<(i+2);k++){
+					for(kk=(j-1);kk<(j+2);kk++){
+						if(campo[k][kk]==MINA){
+							conta++;
+						}
+					}
+				}*/
+				conta++;
+				if(conta!='0'){
+					//campo[i][j]=conta;
+					campo[0][0]=conta;
+				}
+			}
+		}
+	}	
 }
 
 
@@ -51,8 +77,8 @@ void stampaCampo(char** campo,int r,int c){
 }
 
 
-char** inserisciMine(char** campo,int r,int c,int m){
-	int i,j,ran1,ran2,contm=0;
+void inserisciMine(char** campo,int r,int c,int m){ /*Da implemetare in maniera piu efficiente?*/
+	int i,j,ran1,ran2,contm;
 	srand(time(NULL));
 	/*for(i=0;i<r && contm<=m;i++){
 		for(j=0;j<c && contm<=m;j++){
@@ -64,39 +90,72 @@ char** inserisciMine(char** campo,int r,int c,int m){
 			}							
 		}
 	}*/	
+	contm=0;
 	while(contm<m){
 		ran1=rand()%(r);
 		ran2=rand()%(c);
-		if(campo[ran1][ran2]!='*'){
-			campo[ran1][ran2]='*';
+		if(campo[ran1][ran2]!=MINA){
+			campo[ran1][ran2]=MINA;
 			contm++;
 		}		
 	}
-	return campo;
+	//return campo;
 }
 
 
+void turnoGiocatore(char** campo, char**campoHidden,int r,int c,int x,int y,int* loose){
+	if(campo[y-1][x-1]==MINA){
+		printf("Hai preso una mina stupido\n");
+		*loose=1;
+	}
+	else{
+		printf("Hai scoperto con successo la casella in posizione[%d,%d]\n",y, x);
+		campoHidden[y-1][x-1]='O';
+	}
+}
+
+/*--------------------------------------------------------MAIN--------------------------------------*/
 int main(){
 	int scelta=0;
 	char** CampoMain;
+	char** CampoHidden;
 	printf("***---***CampoMinato***---***\n");
 	printf("Cosa vuoi fare?\n1 - Generare schema tramite input\n2 - Generare schema tramite file\n3 - Uscita\n");
 	scanf("%d",&scelta);
 	if (scelta==1){
-		int R,C,mine;
+		int R,C,mine,X,Y,perso=0;
 		do{
 			printf("Dammi la dimensione del campo ((R C) > 0)\n");
 			scanf("%d %d",&R, &C);
 		}while(R<=0 || C<=0);		
 		CampoMain=creaCampo(CampoMain,R,C);
-		CampoMain=riempiCampo(CampoMain,R,C);				
+		CampoHidden=creaCampo(CampoHidden,R,C);
+		CampoMain=riempiCampo(CampoMain,R,C,VUOTO);
+		CampoHidden=riempiCampo(CampoHidden,R,C,'X');				
 		stampaCampo(CampoMain,R,C);
+		stampaCampo(CampoHidden,R,C);
 		do{
-			printf("Dammi il numero di mine che vuoi inserire (0<n<dimensione campo -1)\n");
+			printf("Dammi il numero di mine che vuoi inserire (0<mine<dimensione campo -1[=%d])\n",(R*C)-1);
 			scanf("%d",&mine);
 		}while(mine<=0 || mine>(R*C)-1);
-		CampoMain=inserisciMine(CampoMain,R,C,mine);
+		inserisciMine(CampoMain,R,C,mine);
+		aggiungiNumeri(CampoMain,R,C);/*------------------------------------------------------*/
 		stampaCampo(CampoMain,R,C);
+		stampaCampo(CampoHidden,R,C);
+		scelta=0;
+		do{
+			printf("Ora cosa vuoi fare?\n1 - Scoprire una cella\n2 - Marcare o smarcare una cella\n3 - Terminare il gioco\n");
+			scanf("%d",&scelta);
+			if(scelta==1){
+				printf("Dammi le coordinate della cella che intendi scoprire (PRIMA riga POI colonna)\n");
+				scanf("%d %d",&Y, &X);
+				turnoGiocatore(CampoMain,CampoHidden,R,C,X,Y,&perso);
+				if(perso==0){
+					stampaCampo(CampoMain,R,C);
+					stampaCampo(CampoHidden,R,C);
+				}				
+			}
+		}while(perso==0);
 	}
 	return 0;
 }
